5e55807af0e09c688d5ab26e2450281132f5519e
Dan Loewenherz <dan@lionheartsw.com>
2021-04-02 19:18:41 -0500

    update

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -1,11 +1,11 @@
 // useState: tic tac toe
 // http://localhost:3000/isolated/exercise/04.js
 
-import * as React from 'react'
+import * as React from 'react';
 
-function Board() {
+const Board = () => {
   // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
+  const squares = Array(9).fill(null);
 
   // 🐨 We'll need the following bits of derived state:
   // - nextValue ('X' or 'O')
@@ -16,7 +16,7 @@ function Board() {
 
   // This is the function your square click handler will call. `square` should
   // be an index. So if they click the center square, this will be `4`.
-  function selectSquare(square) {
+  const selectSquare = (square) => {
     // 🐨 first, if there's already winner or there's already a value at the
     // given square index (like someone clicked a square that's already been
     // clicked), then return early so we don't make any state changes
@@ -31,75 +31,70 @@ function Board() {
     // 💰 `squaresCopy[square] = nextValue`
     //
     // 🐨 set the squares to your copy
-  }
+  };
 
-  function restart() {
+  const restart = () => {
     // 🐨 reset the squares
     // 💰 `Array(9).fill(null)` will do it!
-  }
+  };
 
-  function renderSquare(i) {
-    return (
-      <button className="square" onClick={() => selectSquare(i)}>
-        {squares[i]}
-      </button>
-    )
-  }
+  const renderSquare = (i) => (
+    <button className='square' onClick={() => selectSquare(i)}>
+      {squares[i]}
+    </button>
+  );
 
   return (
     <div>
       {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
-      <div className="board-row">
+      <div className='status'>STATUS</div>
+      <div className='board-row'>
         {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
       </div>
-      <div className="board-row">
+      <div className='board-row'>
         {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
       </div>
-      <div className="board-row">
+      <div className='board-row'>
         {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
-      <button className="restart" onClick={restart}>
+      <button className='restart' onClick={restart}>
         restart
       </button>
     </div>
-  )
-}
+  );
+};
 
-function Game() {
-  return (
-    <div className="game">
-      <div className="game-board">
-        <Board />
-      </div>
+const Game = () => (
+  <div className='game'>
+    <div className='game-board'>
+      <Board />
     </div>
-  )
-}
+  </div>
+);
 
 // eslint-disable-next-line no-unused-vars
-function calculateStatus(winner, squares, nextValue) {
-  return winner
+const calculateStatus = (winner, squares, nextValue) =>
+  winner
     ? `Winner: ${winner}`
     : squares.every(Boolean)
     ? `Scratch: Cat's game`
-    : `Next player: ${nextValue}`
-}
+    : `Next player: ${nextValue}`;
 
 // eslint-disable-next-line no-unused-vars
 function calculateNextValue(squares) {
-  const xSquaresCount = squares.filter(r => r === 'X').length
-  const oSquaresCount = squares.filter(r => r === 'O').length
-  return oSquaresCount === xSquaresCount ? 'X' : 'O'
+  const xSquaresCount = squares.filter((r) => r === 'X').length;
+  const oSquaresCount = squares.filter((r) => r === 'O').length;
+  return oSquaresCount === xSquaresCount ? 'X' : 'O';
 }
 
 // eslint-disable-next-line no-unused-vars
-function calculateWinner(squares) {
+const calculateWinner = (squares) => {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -109,18 +104,23 @@ function calculateWinner(squares) {
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-  ]
+  ];
+
   for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i]
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
+    const [a, b, c] = lines[i];
+    if (
+      squares[a] &&
+      squares[a] === squares[b] &&
+      squares[a] === squares[c]
+    ) {
+      return squares[a];
     }
   }
-  return null
-}
+  return null;
+};
 
 function App() {
-  return <Game />
+  return <Game />;
 }
 
-export default App
+export default App;
